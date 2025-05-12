c35b9ba0c9d12f3afad940ed9754f4ce6153c7b3
EzequielEche <eche_@hotmail.com>
2025-05-12 15:42:26 -0300

    probando css para tarjeta de pizza

--- src/componentes/Menu/Menu.jsx
+++ src/componentes/Menu/Menu.jsx
@@ -1,12 +1,14 @@
 import Pizza from "../Pizza/Pizza";
+import "./Menu.css";
 
 const Menu = ({ pedidos }) => {
   return (
     <div>
       {pedidos.length > 0 &&
         pedidos.map((pedido) => {
-          return (
-            <div>
+          return (console.log(pedido),
+            <div key={pedido.id}>
+              
               <Pizza pedido={pedido} />
             </div>
           );
--- src/componentes/Pizza/Pizza.jsx
+++ src/componentes/Pizza/Pizza.jsx
@@ -1,9 +1,14 @@
-const Pizza = ({pedido}) => (
-  <div>
-    <h1> {pedido.nombre} </h1>
-    <h2> {"$" + pedido.precio} </h2>
-    <h3> {"ingredientes: " + pedido.ingredientes} </h3>
+import "./Pizza.css";
+const Pizza = ({pedido}) =>
+   (<div className="pizza-store">
+   <div className="pizza-card">
+    <h1 > {pedido.nombre} </h1>
+    <img src={pedido.imagen} alt={pedido.nombre} />
+    <p > {"$" + pedido.precio} </p>
+    <p > {"ingredientes: " + pedido.ingredientes} </p>
+  </div>
   </div>
 );
+
 
 export default Pizza;
