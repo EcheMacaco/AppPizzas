700809b584f4b77d0a91d3c9d13f7df73f2178e8
Ezequiel Sanchez <echecine@gmail.com>
2025-05-23 15:13:24 -0300

    revision de codigo y comentarios

--- src/componentes/Pizza/Pizza.jsx
+++ src/componentes/Pizza/Pizza.jsx
@@ -1,19 +1,15 @@
 import "./Pizza.css";
 import { pizzaImg } from "../../Img/Img.jsx";
 
-
-const Pizza = ({pizzaItem}) =>
-   (
-   <div className="pizza-card">
-    <h1 > {pizzaItem.nombre} </h1>
+const Pizza = ({ pizzaItem }) => (
+  <div className="pizza-card">
+    <h1> {pizzaItem.nombre} </h1>
     <img src={pizzaImg[pizzaItem.nombre]} alt={pizzaItem.nombre} />
-    <p > {"$" + pizzaItem.precio} </p>
-    <p > {"ingredientes: " + pizzaItem.ingredientes} </p>
-  
+    <p> {"$" + pizzaItem.precio} </p>
+    <p> {"ingredientes: " + pizzaItem.ingredientes} </p>
   </div>
 );
 
 // Pizza es un componente que recibe un objeto pizzaItem como props y muestra su nombre, imagen, precio e ingredientes.
-
 
 export default Pizza;
